05bb5c2febcc424ea96c182ca17c52b7d7144a50
J2021T <80201567+J2021T@users.noreply.github.com>
2021-08-12 17:45:58 -0400

    added thought schema and set up routes directory

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,66 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
-const ThoughtSchema = new Schema();
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: [280, "Getting a little longwinded, lets trim it up."]
+        },
+        username: {
+            type: String,
+            required: "Need to know who is reacting"
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: [6, "Need a bit more for a complete thought don't you think?"],
+            maxLength: [280, "Ok this is becoming more than a thought. Summarize a bit."]            
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: "Need to know who is thinking this thought"
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
